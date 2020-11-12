f440e2f5f2df1fdc983c142a95f47af17e646bf7
euzl <euzl@khu.ac.kr>
2020-11-12 21:08:55 +0900

    feat: 로그인 시 멘토 화면으로 전환

--- app/src/main/java/com/khupearl/smp/LoginActivity.java
+++ app/src/main/java/com/khupearl/smp/LoginActivity.java
@@ -1,18 +1,38 @@
 package com.khupearl.smp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.khupearl.smp.mentor.team.MenteeListActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText editText_ID, editText_password;
+    private Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_login);
         editText_ID = findViewById(R.id.editTextTextEmailAddress);
         editText_password = findViewById(R.id.editTextTextPassword);
 
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_button:
+                startActivity(new Intent(LoginActivity.this, MenteeListActivity.class));
+                Toast.makeText(LoginActivity.this, "멘토로 로그인되었습니다.", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
