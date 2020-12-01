953da34a04a7f9c685966cdbf488f08ba19a087f
euzl <euzl@khu.ac.kr>
2020-12-01 21:06:05 +0900

    feat: 멘티화면에서 wbs 연결

--- app/src/main/java/com/khupearl/smp/mentee/MenteeMainActivity.java
+++ app/src/main/java/com/khupearl/smp/mentee/MenteeMainActivity.java
@@ -1,15 +1,18 @@
 package com.khupearl.smp.mentee;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.khupearl.smp.R;
 import com.khupearl.smp.databinding.ActivityMenteeMainBinding;
+import com.khupearl.smp.wbs.WbsListActivity;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
-public class MenteeMainActivity extends AppCompatActivity {
+public class MenteeMainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ActivityMenteeMainBinding binding;
 
@@ -17,7 +20,18 @@ public class MenteeMainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_mentee_main);
+
+        binding.summaryConstraintlayout.setOnClickListener(this);
     }
 
 
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.summaryConstraintlayout:
+                startActivity(new Intent(MenteeMainActivity.this, WbsListActivity.class));
+                break;
+        }
+
+    }
 }
