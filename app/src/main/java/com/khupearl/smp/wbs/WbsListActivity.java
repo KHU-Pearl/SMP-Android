7194249dddb4b2c3b647dda60997feac9c5e5097
euzl <euzl@khu.ac.kr>
2020-12-04 05:16:22 +0900

    feat: ViewPager 설정
    tab layout과 view pager 을 연결해서 예정/진행/완료 왔다갔다

--- app/src/main/java/com/khupearl/smp/wbs/WbsListActivity.java
+++ app/src/main/java/com/khupearl/smp/wbs/WbsListActivity.java
@@ -5,17 +5,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.khupearl.smp.SmpToolbar;
 import com.khupearl.smp.api.ApiClient;
 import com.khupearl.smp.api.ApiInterface;
 import com.khupearl.smp.R;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -23,13 +31,17 @@ import retrofit2.Response;
 public class WbsListActivity extends AppCompatActivity {
     private static final String TAG = "WbsListActivity";
 
+    private static final int NUM_PAGES = 3; // fragment 개수
+    private ViewPager2 viewPager;
+    private FragmentStateAdapter pagerAdapter; // pager adapter
+
     private SmpToolbar toolbar;
 
-    RecyclerView wbsListRecyclerView;
-    LinearLayoutManager linearLayoutManager;
-    WorkAdapter workAdapter;
+    private RecyclerView wbsListRecyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private WorkAdapter workAdapter;
 
-    ArrayList<Work> workArrayList= new ArrayList<>();
+    private ArrayList<Work> workArrayList= new ArrayList<>();
 
     private String teamName;
 
@@ -45,18 +57,33 @@ public class WbsListActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        wbsListRecyclerView = findViewById(R.id.recyclerview_wbslist);
         toolbar = findViewById(R.id.wbsListToolbar);
-
         setToolBar();
 
-        linearLayoutManager = new LinearLayoutManager(this);
-        wbsListRecyclerView.setLayoutManager(linearLayoutManager);
+        // view pager & tab layout 연결
+        viewPager = findViewById(R.id.viewpager_wbs);
+        pagerAdapter = new WbsPagerAdapter(this);;
+        viewPager.setAdapter(pagerAdapter);
 
-        getWorkList(teamName);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout_wbs);
+        final List<String> tabElement = Arrays.asList("예정", "진행", "완료");
+        new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(tabElement.get(position));
+            }
+        }).attach();
 
-        workAdapter = new WorkAdapter(this, workArrayList);
-        wbsListRecyclerView.setAdapter(workAdapter);
+//        wbsListRecyclerView = findViewById(R.id.recyclerview_wbslist);
+
+
+//        linearLayoutManager = new LinearLayoutManager(this);
+//        wbsListRecyclerView.setLayoutManager(linearLayoutManager);
+//
+//        getWorkList(teamName);
+//
+//        workAdapter = new WorkAdapter(this, workArrayList);
+//        wbsListRecyclerView.setAdapter(workAdapter);
     }
 
     private void getWorkList(final String team) {
@@ -91,5 +118,31 @@ public class WbsListActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    /**
+     * pager adapter
+     */
+    private class WbsPagerAdapter extends FragmentStateAdapter {
+
+        ArrayList<Fragment> fragments = new ArrayList<>();
+
+        public WbsPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+            fragments.add(new TodoFragment());
+            fragments.add(new InprogressFragment());
+            fragments.add(new DoneFragment());
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return NUM_PAGES;
+        }
     }
 }
